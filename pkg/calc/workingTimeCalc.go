beaa2c37d25ff0123dd3624541e4a4b73785eb12
take-2405 <take12.wa@icloud.com>
2020-08-12 19:48:29 +0900

    [ADD]package cala

--- pkg/calc/workingTimeCalc.go
+++ pkg/calc/workingTimeCalc.go
@@ -1,5 +1,20 @@
 package calc
 
-func WorkingTimeCalc(){
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
+func WorkingTimeCalc()gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// リクエストヘッダからX-API-TOKEN(認証トークン)を取得
+		key := "X-API-TOKEN"
+		token := c.GetHeader(key)
+		if len(token) == 0 {
+			log.Println("X-API-TOKEN is empty")
+			return
+		}
+
+			//データを持ってくる
+	}
 }
